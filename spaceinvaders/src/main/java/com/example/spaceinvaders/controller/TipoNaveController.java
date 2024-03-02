8d38f20e5a620e9e59869da9cc3e56e09ce5e382
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2024-03-01 19:30:44 -0500

    Lista tipoNave

--- spaceinvaders/src/main/java/com/example/spaceinvaders/controller/TipoNaveController.java
+++ spaceinvaders/src/main/java/com/example/spaceinvaders/controller/TipoNaveController.java
@@ -1,54 +1,35 @@
 package com.example.spaceinvaders.controller;
 
-import com.example.spaceinvaders.model.TipoNave;
-import com.example.spaceinvaders.services.TipoNaveService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
-@RestController
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.spaceinvaders.model.TipoNave;
+import com.example.spaceinvaders.services.TipoNaveService;
+
+@Controller
 @RequestMapping("/tipoNave")
 public class TipoNaveController {
 
-    private final TipoNaveService tipoNaveService;
+    Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    public TipoNaveController(TipoNaveService tipoNaveService) {
-        this.tipoNaveService = tipoNaveService;
-    }
+    private TipoNaveService tipoNaveService;
 
-    //localhost:8080/tipoNave/listar
-    @GetMapping
-    public List<TipoNave> findAll() {
-        return tipoNaveService.getAll();
-    }
-
-    //localhost:8080/tipoNave/buscarPorId/x
-    @GetMapping("buscarPorId/{id}")
-    public TipoNave findById(@PathVariable Long id) {
-        return tipoNaveService.getById(id);
-    }
-
-    //http://localhost:8080/tipoNave/crear/?nombre="tipoNaveX"&imagen="urlX"
-    @PostMapping("/crear")
-    public TipoNave create(@RequestParam("tipo") String tipo, @RequestParam("volBodega") float volBodega,@RequestParam("velocidad") float velocidad, @RequestParam("foto") String foto) {
-        TipoNave newTipoNave = new TipoNave(tipo,volBodega,velocidad,foto);
-        return tipoNaveService.create(newTipoNave);
-    }
-
-    //http://localhost:8080/tipoNave/actualizar/id?nombre="tipoNaveX"&imagen="urlX"
-    @PutMapping("/actualizar/{id}")
-    public TipoNave update(@PathVariable Long id, @RequestParam("tipo") String tipo, @RequestParam("volBodega") float volBodega,@RequestParam("velocidad") float velocidad, @RequestParam("foto") String foto) {
-        TipoNave tipoNave = new TipoNave(tipo,volBodega,velocidad,foto);
-        return tipoNaveService.update(id, tipoNave);
-    }
-    
-    //localhost:8080/tipoNave/borrar/id
-    @DeleteMapping("/borrar/{id}")
-    public void delete(@PathVariable Long id) {
-        tipoNaveService.delete(id);
+    @GetMapping("/list")
+    public String listarPersonas(Model model) {
+        List<TipoNave> naves = tipoNaveService.listarTipoNaves();
+        model.addAttribute("tipoNave", naves);
+        return "tipoNave-list";
     }
 }
-
-//La herramienta postman es muy util para ver que funcionen estas rutas
