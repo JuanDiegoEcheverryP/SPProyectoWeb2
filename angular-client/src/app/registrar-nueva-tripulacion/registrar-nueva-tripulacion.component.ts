a0012cb6d57eeb9cccee178eaf2ef8faed66ea48
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2024-03-28 01:56:41 -0500

    Pantalla registrar Nueva Tripulacion hecha

--- angular-client/src/app/registrar-nueva-tripulacion/registrar-nueva-tripulacion.component.ts
+++ angular-client/src/app/registrar-nueva-tripulacion/registrar-nueva-tripulacion.component.ts
@@ -12,12 +12,47 @@ import { tipoNaveService } from '../shared/tipoNave.service';
 export class RegistrarNuevaTripulacionComponent {
   
   naves: TipoNave[] = [];
+  actual: TipoNave = new TipoNave(1,"ss",1,1,"dd");
+  indice:number = -1;
 
   constructor(
     private naveService: tipoNaveService,
   ) { }
 
   ngOnInit(): void {
-    this.naveService.listarTipoNaves().subscribe(naves => this.naves = naves)
+    //this.naveService.listarTipoNaves().subscribe(naves => this.naves = naves)
+    this.naveService.listarTipoNaves().subscribe(naves => {
+      this.naves = naves
+      this.actual = this.naves[0];
+      this.indice = 0;
+      console.log(this.actual);
+      
+    });
+  }
+
+  siguienteNave() {
+    console.log(this.indice);
+    
+    if(this.indice == this.naves.length-1) {
+      this.actual = this.naves[0];
+      this.indice = 0;
+    }
+    else {
+      this.actual = this.naves[this.indice+1];
+      this.indice += 1;
+    }
+  }
+
+  anteriorNave() {
+    console.log(this.indice);
+    
+    if(this.indice == 0) {
+      this.actual = this.naves[this.naves.length-1];
+      this.indice = this.naves.length-1;
+    }
+    else {
+      this.actual = this.naves[this.indice-1];
+      this.indice -= 1;
+    }
   }
 }
