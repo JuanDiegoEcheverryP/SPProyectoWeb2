ad21e27287426a6b480a8319a17db87e3d5e8acf
Sophia Aristizabal <saristizabal1017@gmail.com>
2024-04-18 23:48:39 -0500

    puse lo inicial de las pruebas

--- spaceinvaders/src/test/java/com/example/spaceinvaders/SpaceinvadersApplicationTests.java
+++ spaceinvaders/src/test/java/com/example/spaceinvaders/SpaceinvadersApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpaceinvadersApplicationTests {
 
+	private static final String SERVER_URL = "http://localhost:8081";
+	
 	@Test
 	void contextLoads() {
 	}
